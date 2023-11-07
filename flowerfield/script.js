65ad63b694e255fb7269898fb88a6d635eb126de
rosadevera <dever014@newschool.edu>
2023-11-07 18:09:11 -0500

    big publish

---
+++ flowerfield/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const flowerbox = document.querySelector('.flowerbox');
+
+//     flowerbox.addEventListener('mouseover', () => {
+//         flowerbox.classList.add('hovered');
+//     });
+
+//     flowerbox.addEventListener('mouseout', () => {
+//         setTimeout(() => {
+//             flowerbox.classList.remove('hovered');
+//         }, 9000);
+//     });
+// });
