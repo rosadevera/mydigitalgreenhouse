dc618628aaea9ae123a32ea6a5adede6b1dfd9c8
rosadevera <dever014@newschool.edu>
2023-12-11 01:14:36 -0500

    dec 11

--- carelog/script.js
+++ carelog/script.js
@@ -1,21 +1,77 @@
 $("document").ready(function() {
 
-  all_notes.on("keyup", function () {
-    note_content = $(this).text();
-    item_key = "list_" + $(this).index();
-    data = { content: note_content };
-    window.localStorage.setItem(item_key, JSON.stringify(data));
-});
-
-all_notes.each(function (index) {
-    data = JSON.parse(window.localStorage.getItem("list_" + index));
-    if (data !== null) {
-        note_content = data.content;
-        $(this).text(note_content);
+    // Function to save content to local storage
+    function saveContent(key, content) {
+        localStorage.setItem(key, content);
     }
-});
 
-  
-    
+    // Function to retrieve content from local storage
+    function getContent(key) {
+        return localStorage.getItem(key);
+    }
+
+    // Make text within elements with class "editable" editable
+    $(".notifcations1").each(function() {
+        const key = $(this).data("key");
+        const content = getContent(key);
+
+        if (content) {
+            $(this).html(content);
+        }
+
+        $(this).on("click", function() {
+            $(this).attr("contenteditable", true).focus();
+        });
+
+        $(this).on("input", function() {
+            saveContent(key, $(this).html());
+        });
+
+        $(this).on("blur", function() {
+            $(this).removeAttr("contenteditable");
+        });
+    });
+
+    $(".notifcations2").each(function() {
+        const key = $(this).data("key");
+        const content = getContent(key);
+
+        if (content) {
+            $(this).html(content);
+        }
+
+        $(this).on("click", function() {
+            $(this).attr("contenteditable", true).focus();
+        });
+
+        $(this).on("input", function() {
+            saveContent(key, $(this).html());
+        });
+
+        $(this).on("blur", function() {
+            $(this).removeAttr("contenteditable");
+        });
+    });
+
+    $(".notifcations3").each(function() {
+        const key = $(this).data("key");
+        const content = getContent(key);
+
+        if (content) {
+            $(this).html(content);
+        }
+
+        $(this).on("click", function() {
+            $(this).attr("contenteditable", true).focus();
+        });
+
+        $(this).on("input", function() {
+            saveContent(key, $(this).html());
+        });
+
+        $(this).on("blur", function() {
+            $(this).removeAttr("contenteditable");
+        });
+    });
 
 });
