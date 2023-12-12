918a5bfd8b3887e35c0e1cbbb8ddf6ba0ec99bdd
rosadevera <dever014@newschool.edu>
2023-12-12 09:21:58 -0500

    dec12

--- pilea/script.js
+++ pilea/script.js
@@ -20,4 +20,77 @@ $("document").ready(function() {
   musicToggle.toggleClass('paused', audio.paused);
 });
 
+
+function popUpImage() {
+  const image = document.getElementById('popup-image');
+
+  // Increase opacity to make the image "pop up"
+  let opacity = 0;
+  const interval = setInterval(() => {
+    opacity += 0.1;
+    image.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(interval); // Stop the interval when opacity reaches 1
+    }
+  }, 100); // Adjust the interval duration as needed
+}
+
+// Call the popUpImage function after a delay (e.g., 2 seconds)
+setTimeout(popUpImage, 4000);
+
+function popUpImage2() {
+  const image = document.getElementById('popup-image2');
+
+  // Increase opacity to make the image "pop up"
+  let opacity = 0;
+  const interval = setInterval(() => {
+    opacity += 0.1;
+    image.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(interval); // Stop the interval when opacity reaches 1
+    }
+  }, 100); // Adjust the interval duration as needed
+}
+
+// Call the popUpImage function after a delay (e.g., 2 seconds)
+setTimeout(popUpImage2, 8000);
+
+function popUpImage3() {
+  const image = document.getElementById('popup-image3');
+
+  // Increase opacity to make the image "pop up"
+  let opacity = 0;
+  const interval = setInterval(() => {
+    opacity += 0.1;
+    image.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(interval); // Stop the interval when opacity reaches 1
+    }
+  }, 100); // Adjust the interval duration as needed
+}
+
+// Call the popUpImage function after a delay (e.g., 2 seconds)
+setTimeout(popUpImage3, 12000);
+
+function popUpImage4() {
+  const image = document.getElementById('popup-image4');
+
+  // Increase opacity to make the image "pop up"
+  let opacity = 0;
+  const interval = setInterval(() => {
+    opacity += 0.1;
+    image.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(interval); // Stop the interval when opacity reaches 1
+    }
+  }, 100); // Adjust the interval duration as needed
+}
+
+// Call the popUpImage function after a delay (e.g., 2 seconds)
+setTimeout(popUpImage4, 16000);
+
 });
